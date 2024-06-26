d6dfc219ff458bf8624635c1f8d4d0b57939c2f9
Rafael Luis <rafaeluisx@gmail.com>
2024-06-25 22:53:32 -0300

    refactor: refactor the api and its test

--- src/api.ts
+++ src/api.ts
@@ -2,68 +2,61 @@ import crypto from "crypto";
 import express from "express";
 import pgp from "pg-promise";
 import { validate } from "./validateCpf";
+import pg from "pg-promise/typescript/pg-subset";
 const app = express();
 app.use(express.json());
 
 app.post("/signup", async function (req, res) {
-	let result;
-	const connection = pgp()("postgres://postgres:123456@localhost:5432/app");
+	const connection = getConnection();
 	try {
-		const id = crypto.randomUUID();
-
-		const [acc] = await connection.query("select * from cccat15.account where email = $1", [req.body.email]);
-		if (!acc) {
-
-			if (req.body.name.match(/[a-zA-Z] [a-zA-Z]+/)) {
-				if (req.body.email.match(/^(.+)@(.+)$/)) {
-
-					if (validate(req.body.cpf)) {
-						if (req.body.isDriver) {
-							if (req.body.carPlate.match(/[A-Z]{3}[0-9]{4}/)) {
-								await connection.query("insert into cccat15.account (account_id, name, email, cpf, car_plate, is_passenger, is_driver) values ($1, $2, $3, $4, $5, $6, $7)", [id, req.body.name, req.body.email, req.body.cpf, req.body.carPlate, !!req.body.isPassenger, !!req.body.isDriver]);
-								
-								const obj = {
-									accountId: id
-								};
-								result = obj;
-							} else {
-								// invalid car plate
-								result = -5;
-							}
-						} else {
-							await connection.query("insert into cccat15.account (account_id, name, email, cpf, car_plate, is_passenger, is_driver) values ($1, $2, $3, $4, $5, $6, $7)", [id, req.body.name, req.body.email, req.body.cpf, req.body.carPlate, !!req.body.isPassenger, !!req.body.isDriver]);
-
-							const obj = {
-								accountId: id
-							};
-							result = obj;
-						}
-					} else {
-						// invalid cpf
-						result = -1;
-					}
-				} else {
-					// invalid email
-					result = -2;
-				}
-
-			} else {
-				// invalid name
-				result = -3;
-			}
-
-		} else {
-			// already exists
-			result = -4;
-		}
-		if (typeof result === "number") {
-			res.status(422).send(result + "");
-		} else {
-			res.json(result);
-		}
+		const [emailAlreadyUsed] = await connection.query("select * from cccat16.account where email = $1", [req.body.email]);
+		if (emailAlreadyUsed) return res.status(422).json("error: This e-mail is already registered");
+		if (isInvalidName(req.body.name)) return res.status(422).send("This name is not valid");
+		if (isInvalidEmail(req.body.email)) return res.status(422).send("This e-mail is not valid");
+		if (!validate(req.body.cpf)) return res.status(422).send("This CPF is not valid");
+		if (req.body.isDriver && isInvalidLicensePlate(req.body.carPlate)) return res.status(422).send("This license plate is not valid");
+		res.json(await addAccount(crypto.randomUUID(), req, connection));
 	} finally {
-		await connection.$pool.end();
+		await disposeConnection(connection);
 	}
 });
 
+function getConnection(){
+	return pgp()("postgres://postgres:123456@localhost:5432/app");
+}
+
+async function disposeConnection(con: pgp.IDatabase<{}, pg.IClient>){
+	await con.$pool.end();
+}
+
+function isInvalidName(name : string){
+	return !name.match(/[a-zA-Z] [a-zA-Z]+/);
+}
+
+function isInvalidEmail(email: string) : boolean{
+	return !email.match(/^(.+)@(.+)$/);
+}
+
+async function addAccount(id: crypto.UUID, req: express.Request, connection: pgp.IDatabase<{}, pg.IClient>): Promise<object>{
+	await connection.query("insert into cccat16.account (account_id, name, email, cpf, car_plate, passcode, is_passenger, is_driver) values ($1, $2, $3, $4, $5, $6, $7, $8)", [id, req.body.name, req.body.email, req.body.cpf, req.body.carPlate, req.body.passcode, !!req.body.isPassenger, !!req.body.isDriver]);
+	return {accountId: id};
+}
+
+function isInvalidLicensePlate(plate: string): boolean{
+	return !plate.match(/^[A-Z]{3}[0-9]{4}$/);
+}
+
+app.get("/getAccount/:id", async function (req, res) {
+	const connection = getConnection();
+	try {
+		const id = <crypto.UUID> req.params.id as crypto.UUID;
+		const [account] = await connection.query("SELECT * FROM cccat16.account WHERE account_id = $1", [id]);
+		res.json(account);
+	} catch (error) {
+		console.error("Error:", error);
+	}
+	finally{
+		await disposeConnection(connection);
+	}
+});
 app.listen(3000);
--- test/api.test.ts
+++ test/api.test.ts
@@ -1,16 +1,33 @@
 import axios from "axios";
+import { isParameter } from "typescript";
 
 axios.defaults.validateStatus = function () {
 	return true;
 }
 
-test("Deve criar uma conta para o passageiro", async function () {
+test("Deve criar uma conta válida para o passageiro", async function () {
 	const input = {
 		name: "John Doe",
 		email: `john.doe${Math.random()}@gmail.com`,
 		cpf: "87748248800",
-		isPassenger: true
+		passcode: "abc123",
+		isDriver: false
+	};
+		const output = await axios.post("http://localhost:3000/signup", input);
+		expect(output.status).toBe(200);
+		const outputAccount = await axios.get(`http://localhost:3000/getAccount/${output.data.accountId}`);
+		expect(output.data.accountId).toBe(outputAccount.data.account_id);
+});
+
+test("Deve criar uma conta válida para motorista", async function(){
+	const input = {
+		name: "Marty McFly",
+		email: `marty.mcfly${Math.random()}@gmail.com`,
+		cpf: "97456321558",
+		carPlate: "BTL8827",
+		passcode: "321cba",
+		isDriver: true
 	};
 	const output = await axios.post("http://localhost:3000/signup", input);
-	console.log(output.status, output.data);
+	expect(output.status).toBe(200);
 });
