e2d4d074ba3ba43be73e93fd6734a2f5a2179e78
Rafael Luis <rafaeluisx@gmail.com>
2024-06-26 01:07:58 -0300

    feat: add test 'Should try to add an account with an already used e-mail'

--- test/api.test.ts
+++ test/api.test.ts
@@ -15,10 +15,6 @@ test("Should add a valid passenger account", async function () {
   };
   const output = await axios.post("http://localhost:3000/signup", input);
   expect(output.status).toBe(200);
-  const outputAccount = await axios.get(
-    `http://localhost:3000/getAccount/${output.data.accountId}`
-  );
-  expect(output.data.accountId).toBe(outputAccount.data.account_id);
 });
 
 test("Should add a valid driver account", async function () {
@@ -32,4 +28,29 @@ test("Should add a valid driver account", async function () {
   };
   const output = await axios.post("http://localhost:3000/signup", input);
   expect(output.status).toBe(200);
+});
+
+test("Should try to add an account with an already used e-mail", async function () {
+  const sameEmail: string = `emmett.brown${Math.random()}@gmail.com`;
+  const input = {
+    name: "Emmett Brown",
+    email: sameEmail,
+    cpf: "71428793860",
+    passcode: "dmcdelorian",
+    isDriver: false,
+  };
+  const validAccountOutput = await axios.post(
+    "http://localhost:3000/signup",
+    input
+  );
+  expect(validAccountOutput.status).toBe(200);
+  const invalidAccountOutput = await axios.post(
+    "http://localhost:3000/signup",
+    input
+  );
+  expect(invalidAccountOutput.status).toBe(422);
+  expect(invalidAccountOutput.data.status).toBe("error");
+  expect(invalidAccountOutput.data.message).toBe(
+    "This e-mail is already registered"
+  );
 });
